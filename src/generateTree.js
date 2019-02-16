0a755cda5745c76bd6dceb37fc5514da84c60505
jada88 <jacobdstudy@gmail.com>
2019-02-16 23:33:43 +0200

    tree fix

--- src/generateTree.js
+++ src/generateTree.js
@@ -4,7 +4,6 @@ export default function generateTree() {
         id: 0,
         num: 0,
         name: 'folder0',
-        childs: [1],
         childIds: [1]
       }
     }
@@ -14,7 +13,6 @@ export default function generateTree() {
         num: 1,
         name: 'game-of-thrones-family-tree',
         expanded: false,
-        childs: [2,3,4,5,6,7,8,9],
         childIds: []
     }
     return tree
